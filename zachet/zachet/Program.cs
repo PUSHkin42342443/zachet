f495d38247a698d5f091fe223573f09b11167986
PUSHkin42342443 <alexandderdudarev@gmail.com>
2021-12-30 14:58:09 +0500

    gotovo

---
+++ zachet/zachet/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zachet
+{
+                {
+                f = g % 2;
+
+
+            }
+
+
+
+Console.WriteLine(l);
+
+Console.ReadKey();
+
+        }
+        //static int Function(int k)
+        //{
+        //    int i;
+        //  for (i = 0; i < k; i++ )
+        //  {
+
+        //     i = i * i;
+        //  }
+        //  return i;
+
+
+
+        //}
+
+    }  double f;
+int n = k % 2;
+double i;
+double l = k;
+double c = k;
+for (i = k; i >= l; i = i - f)
+{
+
+    f = i / 2;
+    n = k % 2;
+    k = n;
+    c = k - n;
+
+}
+}
